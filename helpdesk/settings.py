06908e1ff6cb2da0d8522f694c428b778bde79e2
Vivek <vk204409@gmail.com>
2025-08-22 01:50:43 +0530

    updated settings.py to use env variables

--- helpdesk/settings.py
+++ helpdesk/settings.py
@@ -7,6 +7,7 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Load .env (local use)
@@ -129,8 +130,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'aashuknnujiya@gmail.com'
-EMAIL_HOST_PASSWORD = 'wpwixiayzmiwkhxi'
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
+
 
 
 # AI Suggestion Settings
